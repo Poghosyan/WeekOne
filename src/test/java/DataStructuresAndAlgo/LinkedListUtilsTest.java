7a5bbfb383745703e97a8c053598dd4a72b26534
Mher <poghosyanusc@gmail.com>
2017-12-13 20:32:03 -0800

    Initial commit

---
+++ src/test/java/DataStructuresAndAlgo/LinkedListUtilsTest.java
@@ -0,0 +1,25 @@
+package DataStructuresAndAlgo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void insertSorted() {
+    }
+
+    @Test
+    public void removeMaximumValues() {
+    }
+
+    @Test
+    public void containsSubsequence() {
+    }
+}
