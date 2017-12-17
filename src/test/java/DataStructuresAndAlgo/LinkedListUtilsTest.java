998ffbc4bd02f7f774a68eede0ebc45e72a5eab0
Mher <poghosyanusc@gmail.com>
2017-12-17 13:07:43 -0800

    Wrote tests for LinkedListUtils class

--- src/test/java/DataStructuresAndAlgo/LinkedListUtilsTest.java
+++ src/test/java/DataStructuresAndAlgo/LinkedListUtilsTest.java
@@ -3,6 +3,7 @@ package DataStructuresAndAlgo;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -19,9 +20,30 @@ public class LinkedListUtilsTest {
 
     @Test
     public void removeMaximumValues() {
+        LinkedList<String> list = new LinkedList<String>(Arrays.asList("1", "2", "3", "4", "5", "9"));
+        LinkedList<String> repeatList = new LinkedList<String>(Arrays.asList("1", "2", "3", "4", "5", "5", "9"));
+
+        LinkedListUtils.removeMaximumValues(list, 2);
+        assertTrue(list.equals(new LinkedList<String>(Arrays.asList("1", "2", "3", "4"))));
+
+        LinkedListUtils.removeMaximumValues(repeatList, 2);
+        assertTrue(repeatList.equals(new LinkedList<String>(Arrays.asList("1", "2", "3", "4"))));
+
+        LinkedListUtils.removeMaximumValues(list, -1);
+        assertTrue(list.equals(new LinkedList<String>(Arrays.asList("1", "2", "3", "4"))));
     }
 
     @Test
     public void containsSubsequence() {
+        LinkedList<Integer> one = new LinkedList<Integer>(Arrays.asList(1, 2, 3, 4, 6, 9));
+        LinkedList<Integer> isContained = new LinkedList<Integer>(Arrays.asList(2, 3, 4));
+        LinkedList<Integer> notContaind = new LinkedList<Integer>(Arrays.asList(2, 4));
+        boolean shouldBeTrue = LinkedListUtils.containsSubsequence(one, isContained);
+        boolean shouldBeFalse = LinkedListUtils.containsSubsequence(one, notContaind);
+        assertTrue(shouldBeTrue);
+        assertFalse(shouldBeFalse);
+
+        shouldBeFalse = LinkedListUtils.containsSubsequence(one, null);
+        assertFalse(shouldBeFalse);
     }
 }
