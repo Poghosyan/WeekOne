3f274bd9a73e869cdeb090dd521be9b27d2ec1ae
Mher <poghosyanusc@gmail.com>
2017-12-24 19:15:37 -0800

    Implemented isValidHtml and got rid of unnecessary comments

--- src/main/java/QueuesAndStacks/HtmlValidator.java
+++ src/main/java/QueuesAndStacks/HtmlValidator.java
@@ -1,21 +1,32 @@
 package QueuesAndStacks;
 
+import java.util.EmptyStackException;
 import java.util.Queue;
 import java.util.Stack;
-
-/*
- * SD2x Homework #2
- * Implement the method below according to the specification in the assignment description.
- * Please be sure not to change the method signature!
- */
 
 public class HtmlValidator {
 	
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+        HtmlTag current, tagToMatch;
+		Stack<HtmlTag> documentObjectModel = new Stack<HtmlTag>();
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+		while (tags.peek() != null) {
+		    current = tags.remove();
+		    if (current.isOpenTag()) {
+                documentObjectModel.push(current);
+            } else if (!current.isSelfClosing()) {
+		        try {
+                    tagToMatch = documentObjectModel.pop();
+                } catch (EmptyStackException E) {
+		            return null;
+                }
+
+		        if (!current.matches(tagToMatch)) {
+                    break;
+                }
+            }
+        }
+		return documentObjectModel;
 	}
 	
 
